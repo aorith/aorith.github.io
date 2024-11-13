41c2eff66ca7b788c79d071d71c919c5d4169cae
Manuel Sanchez Pinar <aomanu@gmail.com>
2024-11-13 20:05:57 +0100

    style updates

--- themes/mine/assets/js/main.js
+++ themes/mine/assets/js/main.js
@@ -1,3 +1,26 @@
-window.addEventListener("load", function (evt) {
-  const todo = "1";
+// Code block triple-click select all for <pre><code>Code</code></pre>
+let clickCount = 0;
+let clickTimer = null;
+
+document.addEventListener("click", function (event) {
+  clickCount++;
+
+  // Reset click count after a delay
+  if (clickTimer) clearTimeout(clickTimer);
+  clickTimer = setTimeout(() => {
+    clickCount = 0;
+  }, 400);
+
+  if (clickCount === 3) {
+    clickCount = 0;
+
+    let elem = event.target.closest("pre");
+    if (elem) {
+      let range = document.createRange();
+      range.selectNodeContents(elem);
+      let selection = window.getSelection();
+      selection.removeAllRanges();
+      selection.addRange(range);
+    }
+  }
 });
